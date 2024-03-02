69b05d6b192b0ec7846ce67bf5ea5713a4f02da3
Heet-Chanchad <161913483+Heet-Chanchad@users.noreply.github.com>
2024-03-02 05:12:20 +0000

    updated

---
+++ 001/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 9917
+let accountEmail = "heet@gmail.com"
+var accountPassword = "12345"
+accountCity = "Brampton"
+
+// accountId = 5   //This is not allowed as that is a constant
+
+accountEmail = "Something@gmail.com"
+accountPassword = "54321"
+accountCity = "Surat"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+// console.log(accountId);
+
---
+++ 001/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alert(3 + 7) //We are using nodejs and not browser, thus it will not work
+
+console.log("This is JS.")
+
+let name = "Heet"
+let age = 19
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
